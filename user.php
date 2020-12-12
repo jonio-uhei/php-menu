2639eac75b125063a2c639008b5bd77a037bcd36
jonio-uhei <hayata19791218@yahoo.co.jp>
2020-12-12 10:04:05 +0900

    公開アプリのデータ

---
+++ user.php
@@ -0,0 +1,33 @@
+<?php
+    class User{
+        private $name;
+
+        private $gender;
+
+        private $id;
+
+        private static $count = 0;
+
+        public function __construct($name,$gender){
+            $this->name = $name;
+
+            $this->gender = $gender;
+
+            self::$count ++;
+            
+            $this->id = self::$count;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+
+        public function getGender(){
+            return $this->gender;
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+    }
+?>
